56b2118a9cd2c37725f44e50cda0146a558f8ae3
Sunny Kr <sunny@Sunnys-MacBook-Pro.local>
2023-10-23 19:15:12 +0100

    first deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 'use client';
-// import RiveComponent from '@/Components/RiveComponent';
 import Image from 'next/image';
 
 export default function Home() {
@@ -7,7 +6,6 @@ export default function Home() {
     <main className='m-10 w-100 min-h-screen'>
       <div className='flex flex-col items-center justify-center tracking-wide space-y-6 h-screen'>
         <div className="basis-1/2 relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-violet-200 before:dark:opacity-10 after:dark:from-pink-900 after:dark:via-[#7c56dd] after:dark:opacity-40 before:lg:h-[360px] z-[-1]"></div>
-        {/* <RiveComponent /> */}
         <Image
           width={900}
           height={900}
